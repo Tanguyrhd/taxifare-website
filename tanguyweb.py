31a3a0049ffd4322cbbc5b93eb22dbbb2e23b2b5
tanguy richard <tanguy.richard.tar@gmail.com>
2025-05-30 15:58:36 -0400

    finished

--- tanguyweb.py
+++ tanguyweb.py
@@ -2,20 +2,25 @@ import streamlit as st
 import datetime
 import requests
 import pandas as pd
+from streamlit_folium import st_folium
+import folium
 import numpy as np
 
-'''
-# TaxiFareModel
-'''
+st.title('TaxiFare Prediction 🚕😎💸')
+
+st.header('What are the info of the ride you want to predict ?')
 
 
-'''
-## What are the info of the ride you want to predict ?
-'''
-max_value = datetime.date(2016, 1, 1)
+max_value = datetime.date(2020, 1, 1)
 min_value = datetime.date(2009, 1, 1)
-pickup_date = st.date_input("date of pickup", value=None, max_value=max_value, min_value=min_value)
-pickup_time = st.time_input("time of pickup", value=None, step=60)
+
+pickup_date = st.date_input("date of pickup",
+                            value=datetime.date(2015, 1, 1),
+                            max_value=max_value, min_value=min_value)
+pickup_time = st.time_input("time of pickup",
+                            value=datetime.time(12, 0),
+                            step=60)
+
 pickup_longitude = st.number_input(
     "pickup longitude", value=-73.950655, placeholder="Type a longitude...", format="%0.6f"
     )
@@ -26,7 +31,7 @@ dropoff_longitude = st.number_input(
     "dropoff longitude", value=-73.984365, placeholder="Type a longitude...", format="%0.6f"
     )
 dropoff_latitude = st.number_input(
-    "dropoff latitude", value=40.769802, placeholder="Type a longitude...", format="%0.6f"
+    "dropoff latitude", value=40.769802, placeholder="Type a latitude...", format="%0.6f"
     )
 passenger_count = st.number_input(
     "Passenger count",
@@ -37,41 +42,47 @@ passenger_count = st.number_input(
     format="%d"
     )
 
-pickup_datetime = datetime.datetime.combine(pickup_date, pickup_time)
-
-params = {
-'pickup_datetime':pickup_datetime,
-'pickup_longitude':pickup_longitude,
-'pickup_latitude':pickup_latitude,
-'dropoff_longitude':dropoff_longitude,
-'dropoff_latitude':dropoff_latitude,
-'passenger_count':passenger_count,
-}
-
 if st.button("Predict fare"):
+    try:
 
-    url = f'https://taxifare.lewagon.ai/predict'
+        pickup_datetime = datetime.datetime.combine(pickup_date, pickup_time)
 
-    response = requests.get(url, params=params)
-    data = response.json()
+        params = {
+        'pickup_datetime':pickup_datetime,
+        'pickup_longitude':pickup_longitude,
+        'pickup_latitude':pickup_latitude,
+        'dropoff_longitude':dropoff_longitude,
+        'dropoff_latitude':dropoff_latitude,
+        'passenger_count':passenger_count,
+        }
 
-    if response.status_code == 200:
+        url = f'https://taxifare.lewagon.ai/predict'
 
-        st.title(f'This ride will probably cost {np.round(data['fare'], 2)}$')
+        response = requests.get(url, params=params)
 
-        df = pd.DataFrame(
-            {
-                'lon':[pickup_longitude, dropoff_longitude],
-                'lat':[pickup_latitude, dropoff_latitude],
-                'color':["#008000", "#FF0000"]
-            })
+        if response.status_code == 200:
+            data = response.json()
+            fare = round(data["fare"], 2)
 
-        st.title('See on the map :')
+            st.success(f"This ride will probably cost **${fare}** 💰")
 
-        st.markdown('-> pickup location in green :large_green_square:')
-        st.markdown('-> dropoff location in red :large_red_square:')
+            df = pd.DataFrame(
+                {
+                    'lon':[pickup_longitude, dropoff_longitude],
+                    'lat':[pickup_latitude, dropoff_latitude],
+                    'color':["#008000", "#FF0000"]
+                })
+
+            st.title('See on the map :')
+
+            st.markdown('-> pickup location in :green[green] :large_green_square:')
+            st.markdown('-> dropoff location in :red[red] :large_red_square:')
 
 
-        map = st.map(df, color="color", zoom=9)
-    else:
-        st.error(f"Error from API: {response.status_code}\n{response.text}")
+            map = st.map(df, color="color", zoom=9)
+
+        else:
+            st.error(f"Error from API: {response.status_code}\n{response.text}")
+
+    except Exception as e:
+        st.error(f"Unexpected error: {e}")
